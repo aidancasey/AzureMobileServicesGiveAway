1ae679b26bda000d6d4d2ccc144ff5798de48b00
Aidan Casey <aidancasey@gmail.com>
2013-03-22 03:33:46 +1100

    basic Windows 8 app with push notification

---
+++ Website/Models/Feedback.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class Feedback
+    {
+        public int? Id { get; set; }
+        [Required]
+        [MaxLength(140)]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string TwitterHandle { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
